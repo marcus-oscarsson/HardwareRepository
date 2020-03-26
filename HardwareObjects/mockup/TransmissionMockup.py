da23f12717212a7d5e0d91a0c73f32bacb2c0557
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2020-03-26 09:17:29 +0100

    Inheriting AbstractActuator

--- HardwareObjects/mockup/TransmissionMockup.py
+++ HardwareObjects/mockup/TransmissionMockup.py
@@ -1,10 +1,10 @@
-from HardwareRepository.BaseHardwareObjects import Device
+from HardwareRepository.HardwareObjects.abstract.AbstractActuator import (
+    AbstractActuator,
+)
 
-
-class TransmissionMockup(Device):
+class TransmissionMockup(AbstractActuator):
     def __init__(self, name):
-        Device.__init__(self, name)
-
+        super(TransmissionMockup, self).__init__(name)
         self.labels = []
         self.bits = []
         self.attno = 0
@@ -13,30 +13,18 @@ class TransmissionMockup(Device):
     def init(self):
         pass
 
-    def getAttState(self):
-        return 0
-
-    def setAttFactor(self, value):
-        self.value = value
-        self.emit("valueChanged", self.value)
-
     def get_value(self):
         return self.value
 
     def _set_value(self, value):
-        self.setAttFactor(value)
+        self.value = value
+        self.emit("valueChanged", self.value)
 
     def connected(self):
         self.setIsReady(True)
 
     def disconnected(self):
         self.setIsReady(False)
-
-    def attStateChanged(self, channelValue):
-        pass
-
-    def attFactorChanged(self, channelValue):
-        pass
 
     def isReady(self):
         return True
